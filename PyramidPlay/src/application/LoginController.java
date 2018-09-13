ca71c4c7cb3123a2d13e8345f94b76ef1f14d1ba
jakeparcell14 <jakeparcell14@gmail.com>
2018-09-13 11:26:13 -0700

    Created Login Screen
    The login screen is not set to appear on startup just because we are focusing more on the SongView page. The Login and Register page is set up but not yet functional.

--- PyramidPlay/src/application/LoginController.java
+++ PyramidPlay/src/application/LoginController.java
@@ -4,15 +4,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable
 {
@@ -41,6 +47,18 @@ public class LoginController implements Initializable
 	@FXML
 	private Button RegisterOrCancelButton;
 	
+	@FXML
+	private TextField AddFirstNameTextField;
+	
+	@FXML
+	private TextField AddLastNameTextField;
+	
+	@FXML
+	private TextField AddUsernameTextField;
+	
+	@FXML
+	private TextField AddPasswordTextField;
+	
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
@@ -50,20 +68,64 @@ public class LoginController implements Initializable
 	}
 	
 	@FXML
-	public void OnRegisterClicked(MouseEvent event) throws IOException
+	public void OnSignInOrRegisterClicked(ActionEvent event)
 	{
-		System.out.println("Register Button Clicked!");
+		if( SignInOrRegisterButton.getText().equals("Sign In"))
+		{
+			System.out.println("Sign In Pressed");
+		}
+		else
+		{
+			System.out.println("Register Account Pressed");
+		}
 	}
 	
 	@FXML
-	public void OnSignInOrRegisterClicked(MouseEvent event)
+	public void OnRegisterOrCancelClicked(ActionEvent event)
 	{
-		
+		if( SignInOrRegisterButton.getText().equals("Sign In"))
+		{
+			SignInOrRegisterButton.setText("Register Account");
+			RegisterOrCancelButton.setText("Cancel");
+			
+			this.clearSignInText();
+			
+			//hide Sign In Panel
+			SignInPane.setVisible(false);
+			SignInPane.setMouseTransparent(true);
+			
+			//show Register Panel
+			RegisterPane.setVisible(true);
+			RegisterPane.setMouseTransparent(false);
+		}
+		else
+		{
+			SignInOrRegisterButton.setText("Sign In");
+			RegisterOrCancelButton.setText("Create an Account");
+			
+			this.clearRegisterText();
+			
+			//show Sign In Panel
+			SignInPane.setVisible(true);
+			SignInPane.setMouseTransparent(false);
+			
+			//hide Register Panel
+			RegisterPane.setVisible(false);
+			RegisterPane.setMouseTransparent(true);
+		}
 	}
 	
-	@FXML
-	public void OnRegisterOrCancelClicked(MouseEvent event)
+	public void clearSignInText()
 	{
-		
+		UsernameTextField.setText("");
+		PasswordTextField.setText("");
+	}
+	
+	public void clearRegisterText()
+	{
+		AddFirstNameTextField.setText("");
+		AddLastNameTextField.setText("");
+		AddUsernameTextField.setText("");
+		AddPasswordTextField.setText("");
 	}
 }
