485630b5f31e8feb687f9c8703f17595ce13da7a
Wesley Slates <Wesley.Slates@student.csulb.edu>
2018-09-17 22:04:55 -0700

    Removed uneeded imports

--- PyramidPlay/src/application/SongViewController.java
+++ PyramidPlay/src/application/SongViewController.java
@@ -4,19 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.ResourceBundle;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
