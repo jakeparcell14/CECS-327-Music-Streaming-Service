768d7faf18cbd747340b75a20bf891b61d672d6e
Matt <chumatthewp@gmail.com>
2018-10-08 13:06:46 -0700

    Timeouts for signing in

--- PyramidPlay/src/application/LoginController.java
+++ PyramidPlay/src/application/LoginController.java
@@ -26,6 +26,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import com.google.gson.Gson;
 
@@ -92,6 +93,7 @@ public class LoginController implements Initializable
 			
 			//create a socket with no specific port we listen on
 			socket = new DatagramSocket();
+			socket.setSoTimeout(10000); //set timeout for 10 seconds
 			System.out.println("Socket created with port " + socket.getLocalPort());
 /**			
 			//send a connection request to the server
@@ -113,8 +115,6 @@ public class LoginController implements Initializable
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 	
@@ -124,29 +124,36 @@ public class LoginController implements Initializable
 	 * @param password - password entered in the password textfield
 	 * @param event - the event that triggered this function
 	 */
-	public void signIn(String username, String password, ActionEvent event) {
+	public void signIn(String username, String password, ActionEvent event) throws IOException {
 		//create message to send to server
 		String[] arr = {username, password};
 		
 		//initialize buffer
 		byte[] buffer = new byte[1000];
+		Message loginMsg = new Message();
+		
 		try {
-			Message loginMsg = new Message(1, requestID++, OpID.LOGIN, arr, InetAddress.getLocalHost(), 1);
-			
-			//convert to json
-			String json = gson.toJson(loginMsg);
-			
-			//we can only send bytes, so flatten the string to a byte array
-			byte[] msg = gson.toJson(loginMsg).getBytes();				
-			
-			System.out.println("Sending request.");
+			loginMsg = new Message(1, requestID++, OpID.LOGIN, arr, InetAddress.getLocalHost(), 1);
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		//convert to json
+		String json = gson.toJson(loginMsg);
+		
+		//we can only send bytes, so flatten the string to a byte array
+		byte[] msg = gson.toJson(loginMsg).getBytes();				
+		
+		System.out.println("Sending request.");
+		try {
 			//initialize and send request packet using port 1234, the port the server is listening on
 			DatagramPacket request = new DatagramPacket(msg, msg.length, loginMsg.getAddress() , 1234);
 			socket.send(request);
-			System.out.println("request port: " + request.getPort());
-					
+			//catch (SocketTimeoutException to)
+			
 			//initialize reply from server and receive it
-					
+			
 			/* without specifying a port in this datagram packet, the OS will
 			 * randomly assign a port to the reply for the program to listen on
 			 */
@@ -154,8 +161,28 @@ public class LoginController implements Initializable
 			System.out.println("Awaiting response from server...");
 			socket.receive(reply);		
 			System.out.println("Response received from port " + reply.getPort() + "!");
-			System.out.println(gson.fromJson(new String(buffer).trim(), String.class));
-			
+			//System.out.println(gson.fromJson(new String(buffer).trim(), String.class));
+		} catch (SocketTimeoutException e) {
+			for (int i = 0; i < 5; i++) {
+				System.out.println("No response from server, sending request again.");
+				//initialize and send request packet using port 1234, the port the server is listening on
+				DatagramPacket request = new DatagramPacket(msg, msg.length, loginMsg.getAddress() , 1234);
+				socket.send(request);
+				//catch (SocketTimeoutException to)
+				
+				//initialize reply from server and receive it
+				
+				/* without specifying a port in this datagram packet, the OS will
+				 * randomly assign a port to the reply for the program to listen on
+				 */
+				DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+				System.out.println("Awaiting response from server...");
+				socket.receive(reply);		
+				System.out.println("Response received from port " + reply.getPort() + "!");
+			}
+		} 
+		
+		try {
 			//if server responds with acknowledgement "VERIFIED" switch to song view
 			if(gson.fromJson(new String(buffer).trim(), String.class).equals("VERIFIED"))
 			{
@@ -179,9 +206,6 @@ public class LoginController implements Initializable
 			{
 				InvalidSignInLabel.setVisible(true);
 			}
-		} catch (UnknownHostException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -251,16 +275,20 @@ public class LoginController implements Initializable
 	 */
 	public void signInOrRegister(ActionEvent event)
 	{
-		//attempts to sign in the user with the given username and password
-		if( SignInOrRegisterButton.getText().equals("Sign In") )
-		{
-			//user is attempting to sign in
-			signIn(UsernameTextField.getText(), PasswordTextField.getText(), event);
-		}
-		else
-		{
-			//user is attempting to register a new account
-			register(AddFirstNameTextField.getText(), AddLastNameTextField.getText(), AddUsernameTextField.getText(), AddPasswordTextField.getText(), event);
+		try {
+			//attempts to sign in the user with the given username and password
+			if( SignInOrRegisterButton.getText().equals("Sign In") )
+			{
+				//user is attempting to sign in
+				signIn(UsernameTextField.getText(), PasswordTextField.getText(), event);
+			}
+			else
+			{
+				//user is attempting to register a new account
+				register(AddFirstNameTextField.getText(), AddLastNameTextField.getText(), AddUsernameTextField.getText(), AddPasswordTextField.getText(), event);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 
