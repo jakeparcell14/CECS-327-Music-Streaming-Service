c247bcdc6cc1715d4d3593410bea59341496139e
jakeparcell14 <jakeparcell14@gmail.com>
2018-10-14 15:05:21 -0700

    began to implement call semantics

--- PyramidPlay/src/application/Server.java
+++ PyramidPlay/src/application/Server.java
@@ -11,166 +11,163 @@ import com.google.gson.Gson;
 
 public class Server {
 	/**
-     * The appplication main method, which just listens on a port and
-     * spawns handler threads.
-     */
+	 * The appplication main method, which just listens on a port and
+	 * spawns handler threads.
+	 */
 	private ArrayList<Song> allSongs;
-	
+
 	/**
 	 * Socket used to get incoming requests.
 	 */
 	private static DatagramSocket socket = null;
-	
+
 	/**
 	 * GSON object to serialize and deserialize objects.
 	 */
 	private static Gson gson = new Gson();
-	
-	
-    public static void main(String[] args) throws Exception {		
+
+
+	public static void main(String[] args) throws Exception {		
 		try {
 			//create a socket listening on port 1234
 			socket = new DatagramSocket(1234);
-			
+
 			while(true) {
 				System.out.println("Waiting for a request...");
 				Request req = getRequest();
-				
+
 				System.out.println("Received a request!\nCreating new thread!");
 				//create a new thread to handle a client's requests
 				new Handler(req).start();
 			}
 		} catch (SocketException e1) {
-			// TODO Auto-generated catch block
 			System.out.println("Error creating socket: " + e1.getMessage());
 		} finally  {
 			if (socket != null) 
 				socket.close();
 		}
-    }
-    
-    /**
-     * Handler class that will handle a single request from a client.
-     * @author Matthew
-     *
-     */
-    public static class Handler extends Thread {
-    	private Request req;
-    	private DatagramSocket reqSocket;
-    	private Gson gson;
-    	
-    	/**
-    	 * Constructor for Handler class that creates a socket to 
-    	 * handle the request from the client
-    	 * @param req - request from a client.
-    	 */
-    	public Handler(Request req) {
-    		this.req = req;
-    		gson = new Gson();
-    		
-    		//create a new socket to handle requests from the client
-    		try {
+	}
+
+	/**
+	 * Handler class that will handle a single request from a client.
+	 * @author Matthew
+	 *
+	 */
+	public static class Handler extends Thread {
+		private Request req;
+		private DatagramSocket reqSocket;
+		private Gson gson;
+
+		/**
+		 * Constructor for Handler class that creates a socket to 
+		 * handle the request from the client
+		 * @param req - request from a client.
+		 */
+		public Handler(Request req) {
+			this.req = req;
+			gson = new Gson();
+
+			//create a new socket to handle requests from the client
+			try {
 				reqSocket = new DatagramSocket();
 				System.out.println(reqSocket.getLocalPort());
 			} catch (SocketException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    	}
-    	
-    	public void run() {
-    		System.out.println("New handler running and handling request");
-    		System.out.println(new String(req.data).trim());
-    		Message msg = gson.fromJson(new String(req.data).trim(), Message.class);
-    		
-/**    		for (int i = 0; i < msg.getArgs().length; i ++) {
+		}
+
+		public void run() {
+			System.out.println("New handler running and handling request");
+			System.out.println(new String(req.data).trim());
+			Message msg = gson.fromJson(new String(req.data).trim(), Message.class);
+
+			/**    		for (int i = 0; i < msg.getArgs().length; i ++) {
     			System.out.println("args[" + i + "]= " + msg.getArgs()[i]);
     		}
-*/    		
-    		switch(msg.getProtocolID()) {
-    			case 0:
-    				break;
-    			case 1:
-					try {
-						RequestReplyProtocol(msg, req);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-    			
-    		}
-  
-			
-    		//socket should close at the end/destruction of this thread
-    	}
-    	
-    }
-    
-    /**
-     * Handles logic for request-reply protocol.
-     * 
-     * @param msg Message received by the server.
-     * @param req Request data received by server.
-     * @throws SocketException
-     * @throws UnknownHostException
-     * @throws IOException
-     */
-    private static void RequestReplyProtocol(Message msg, Request req) throws SocketException, UnknownHostException, IOException {
+			 */    		
+			switch(msg.getProtocolID()) {
+			case 0:
+				break;
+			case 1:
+				try {
+					RequestReplyProtocol(msg, req);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+
+			//socket should close at the end/destruction of this thread
+		}
+
+	}
+
+	/**
+	 * Handles logic for request-reply protocol.
+	 * 
+	 * @param msg Message received by the server.
+	 * @param req Request data received by server.
+	 * @throws SocketException
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	private static void RequestReplyProtocol(Message msg, Request req) throws SocketException, UnknownHostException, IOException {
 		byte[] result = ChooseAndExecuteOperation(msg);
 		SendReply(result, InetAddress.getLocalHost(), req.port);
 	}
-    
-    /***
-     * Chooses an operation based on the operation id in the message object sent to the server.
-     * 
-     * @param msg Message the server received.
-     * @return Returns result in the form of a flattened byte array ready to be sent.
-     * @throws IOException
-     */  
-    private static byte[] ChooseAndExecuteOperation(Message msg) throws IOException {
-    	switch(msg.getOperationID()) {
-			case LOGIN:
-				return verifyAccount(msg);
-			case REGISTER:
-				return registerAccount(msg);
-			case SEARCHALLSONGS:
-				return searchAllSongs(msg);
-			case SEARCHMYSONGS:
-				//searchMySongs function goes here
-				return searchMySongs(msg);
-			case SEARCHMYPLAYLISTS:
-				return searchMyPlaylists(msg);
-				//searchMyPlaylists function goes here
-			case SEARCHCURRENTPLAYLIST:
-				return searchCurrentPlaylist(msg);
-				//searchCurrentPlaylist function goes here
-			case ADDPLAYLIST:
-				return addPlaylist(msg);
-			case DELETEPLAYLIST:
-				return deletePlaylist(msg);
-			case ADDSONGTOPLAYLIST:
-				return addSong(msg);
-			case DELETESONGFROMPLAYLIST:
-				return deleteSong(msg);
-			default:
-				return null;
-			
-    	}
-    }
- 
-    /**
-     * Executes a search of all songs.
-     * @param m Message sent to the server.
-     * @return Returns result as flattened byte array ready to be sent.
-     */
-    public static byte[] searchAllSongs(Message m) {
-    	ArrayList<Song> allSongs;
-    	Playlist validSongs = new Playlist("valid");
+
+	/***
+	 * Chooses an operation based on the operation id in the message object sent to the server.
+	 * 
+	 * @param msg Message the server received.
+	 * @return Returns result in the form of a flattened byte array ready to be sent.
+	 * @throws IOException
+	 */  
+	private static byte[] ChooseAndExecuteOperation(Message msg) throws IOException {
+		switch(msg.getOperationID()) {
+		case LOGIN:
+			return verifyAccount(msg);
+		case REGISTER:
+			return registerAccount(msg);
+		case SEARCHALLSONGS:
+			return searchAllSongs(msg);
+		case SEARCHMYSONGS:
+			//searchMySongs function goes here
+			return searchMySongs(msg);
+		case SEARCHMYPLAYLISTS:
+			return searchMyPlaylists(msg);
+			//searchMyPlaylists function goes here
+		case SEARCHCURRENTPLAYLIST:
+			return searchCurrentPlaylist(msg);
+			//searchCurrentPlaylist function goes here
+		case ADDPLAYLIST:
+			return addPlaylist(msg);
+		case DELETEPLAYLIST:
+			return deletePlaylist(msg);
+		case ADDSONGTOPLAYLIST:
+			return addSong(msg);
+		case DELETESONGFROMPLAYLIST:
+			return deleteSong(msg);
+		default:
+			return null;
+
+		}
+	}
+
+	/**
+	 * Executes a search of all songs.
+	 * @param m Message sent to the server.
+	 * @return Returns result as flattened byte array ready to be sent.
+	 */
+	public static byte[] searchAllSongs(Message m) {
+		ArrayList<Song> allSongs;
+		Playlist validSongs = new Playlist("valid");
 		try {
 			allSongs = UserRepository.getAllSongs();
-	    	String query=m.getArgs()[1];
-	    	
-	    	for(int i=0; i<allSongs.size();i++) {
+			String query=m.getArgs()[1];
+
+			for(int i=0; i<allSongs.size();i++) {
 				if(validSongs.getSongs().size() <= 20)
 				{
 					//checks if query matches the title of the current song
@@ -208,24 +205,23 @@ public class Server {
 				}
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return gson.toJson(validSongs.getSongs().toArray(new Song[validSongs.getSongs().size()])).getBytes();
-    	
-    }
-    
-    /**
-     * Executes search in a user's my songs library.
-     * 
-     * @param m Message sent to the server.
-     * @return Returns result as flattened byte array ready to be sent.
-     */
-    public static byte[] searchMySongs(Message m) {
-    	String userName=m.getArgs()[0];
-    	String query=m.getArgs()[1];
-    	ArrayList<Song> msgList=new ArrayList<Song>();
-    	User user;
+
+	}
+
+	/**
+	 * Executes search in a user's my songs library.
+	 * 
+	 * @param m Message sent to the server.
+	 * @return Returns result as flattened byte array ready to be sent.
+	 */
+	public static byte[] searchMySongs(Message m) {
+		String userName=m.getArgs()[0];
+		String query=m.getArgs()[1];
+		ArrayList<Song> msgList=new ArrayList<Song>();
+		User user;
 		try {
 			user = UserRepository.getUser(userName);
 			Playlist savedSongsPlaylist=user.getSavedSongs();
@@ -253,7 +249,6 @@ public class Server {
 			}
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return gson.toJson(msgList.toArray(new Song[msgList.size()])).getBytes();
@@ -287,12 +282,11 @@ public class Server {
 			}
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
+
+
 		return gson.toJson(msgList.toArray(new Playlist[msgList.size()])).getBytes();
 	}
 	/**
@@ -301,10 +295,10 @@ public class Server {
 	 */
 	public static byte[] searchCurrentPlaylist(Message m) {
 		String userName=m.getArgs()[0];
-    	String query=m.getArgs()[1];
-    	String currentPlaylist=m.getArgs()[2];
-    	ArrayList<Song> msgList=new ArrayList<Song>();
-    	User user;
+		String query=m.getArgs()[1];
+		String currentPlaylist=m.getArgs()[2];
+		ArrayList<Song> msgList=new ArrayList<Song>();
+		User user;
 		try {
 			user = UserRepository.getUser(userName);
 			ArrayList<Playlist> playlists=user.getPlaylists();
@@ -327,7 +321,7 @@ public class Server {
 				//checks if query matches the title of the current song
 				if(songs.get(i).getTitle()!=null && songs.get(i).getTitle().toLowerCase().contains(query.toLowerCase())) {
 					msgList.add(songs.get(i));			
-					}
+				}
 				//checks if query matches the album of the current song
 				else if(songs.get(i).getAlbum()!=null && songs.get(i).getAlbum().toLowerCase().contains(query.toLowerCase())) {
 					msgList.add(songs.get(i));		
@@ -338,22 +332,21 @@ public class Server {
 				}
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return gson.toJson(msgList.toArray(new Song[msgList.size()])).getBytes();
 	}
-    
+
 	/**
 	 * Function to verify a username and password combination from a message
 	 * @param msg - message sent from client containing username and password
 	 * 				args array should be [username, password]
 	 */
-    public static byte[] verifyAccount(Message msg) {
-    	gson = new Gson();
-    	
-    	try 
+	public static byte[] verifyAccount(Message msg) {
+		gson = new Gson();
+
+		try 
 		{
 			if(UserRepository.IsUsernameAndPasswordCorrect(msg.getArgs()[0], msg.getArgs()[1]))
 			{
@@ -367,30 +360,29 @@ public class Server {
 		} 
 		catch (IOException e) 
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 	}
-    
-    /**
-     * Function to register a new account to the json file
-     * @param msg - message sent from client containing user information
-     */
-    public static byte[] registerAccount(Message msg) {
-    	Gson gson = new Gson();
-    	
-    	//msg args structure = [firstName, lastName, userName, password]
-    	try 
+
+	/**
+	 * Function to register a new account to the json file
+	 * @param msg - message sent from client containing user information
+	 */
+	public static byte[] registerAccount(Message msg) {
+		Gson gson = new Gson();
+
+		//msg args structure = [firstName, lastName, userName, password]
+		try 
 		{
 			if(!UserRepository.userExists(msg.getArgs()[2]))
 			{
 				//username is available and ready to be added to the repository
 				User newUser = new User(msg.getArgs()[0], msg.getArgs()[1], msg.getArgs()[2], msg.getArgs()[3]);
-				
+
 				//add user to the user repository
 				UserRepository.AddUser(newUser);
-				
+
 				//return new user
 				return gson.toJson(newUser).getBytes();
 			}
@@ -406,9 +398,9 @@ public class Server {
 			e.printStackTrace();
 			return null;
 		}
-    }
-    
-    /**
+	}
+
+	/**
 	 * Sends a reply to the client.
 	 * 
 	 * @param reply Object returning as a reply flattened to a byte array.
@@ -426,7 +418,7 @@ public class Server {
 		System.out.printf("Sending Reply. Port %d, InetAddr: %s\n", rply.getPort(), rply.getAddress());		
 		socket.send(rply);
 	}
-	
+
 	/**
 	 * listens for a request from a client.
 	 * @param socket This server's socket.
@@ -447,7 +439,7 @@ public class Server {
 		//socket.close();
 		return req;
 	}
-	
+
 	/**
 	 * Gets all of a user's playlist.
 	 * @param msg Message for this request.
@@ -459,7 +451,7 @@ public class Server {
 		User user = UserRepository.getUser(username);
 		return gson.toJson((Playlist[]) user.getPlaylists().toArray(), Playlist[].class).getBytes();
 	}
-	
+
 	/**
 	 * Adds a playlist to a user account.
 	 * @param msg Message for this request.
@@ -468,11 +460,17 @@ public class Server {
 	 */
 	private static byte[] addPlaylist(Message msg) throws IOException {
 		User user = UserRepository.getUser(msg.getArgs()[0]);
-		user.addPlaylist(gson.fromJson(msg.getArgs()[1], Playlist.class));
-		UserRepository.UpdateUser(user);
+		Playlist p = gson.fromJson(msg.getArgs()[1], Playlist.class);
+
+		// makes method idempotent because the method will react the same regardless of how many times a specific process is called TODO make sure it works
+		if(!user.getPlaylists().contains(p))
+		{
+			user.addPlaylist(p);
+			UserRepository.UpdateUser(user);
+		}
 		return gson.toJson((Playlist[]) user.getPlaylists().toArray(new Playlist[user.getPlaylists().size()])).getBytes();
 	}
-	
+
 	/**
 	 * Deletes a given playlist from a user account.
 	 * 
@@ -482,12 +480,18 @@ public class Server {
 	 */
 	private static byte[] deletePlaylist(Message msg) throws IOException {
 		User user = UserRepository.getUser(msg.getArgs()[0]);
-		user.removePlaylist(gson.fromJson(msg.getArgs()[1], Playlist.class).getPlaylistName());
-		UserRepository.UpdateUser(user);
-		System.out.println(gson.toJson((Playlist[]) user.getPlaylists().toArray(new Playlist[user.getPlaylists().size()])));
+		Playlist p = gson.fromJson(msg.getArgs()[1], Playlist.class);
+
+		// makes method idempotent because the method will react the same regardless of how many times a specific process is called TODO make sure it works
+		if(user.getPlaylists().contains(p))
+		{
+			user.removePlaylist(gson.fromJson(msg.getArgs()[1], Playlist.class).getPlaylistName());
+			UserRepository.UpdateUser(user);
+			System.out.println(gson.toJson((Playlist[]) user.getPlaylists().toArray(new Playlist[user.getPlaylists().size()])));
+		}
 		return gson.toJson((Playlist[]) user.getPlaylists().toArray(new Playlist[user.getPlaylists().size()])).getBytes();
 	}
-	
+
 	/**
 	 * Adds a song to a given playlist.
 	 * 
@@ -521,7 +525,7 @@ public class Server {
 			return gson.toJson((Playlist[]) p.toArray(new Playlist[p.size()]), Playlist[].class).getBytes();
 		}
 	}
-	
+
 	/**
 	 * Deletes the given song from a given playlist.
 	 * 
@@ -550,7 +554,7 @@ public class Server {
 		{
 			//arraylist of playlists from the user
 			ArrayList<Playlist> p = user.getPlaylists();
-			
+
 			//search for playlist by comparing names
 			int playlistIndex = 0;
 			while(!playlist.getPlaylistName().equals( p.get(playlistIndex).getPlaylistName() ) 
