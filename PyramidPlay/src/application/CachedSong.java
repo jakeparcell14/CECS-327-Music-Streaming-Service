660605e12c91438c5ab9992d8527ad1433e662be
jakeparcell14 <jakeparcell14@gmail.com>
2018-11-11 17:10:37 -0800

    added documentation to CachedSong class

--- PyramidPlay/src/application/CachedSong.java
+++ PyramidPlay/src/application/CachedSong.java
@@ -1,11 +1,32 @@
 package application;
 
+/**
+ * This class saves the byte array when the server requests the bytes of the mp3 from a peer
+ * @author Jacob Parcell
+ *
+ */
 public class CachedSong 
 {
+	/**
+	 * Global Unique Identifier for the song
+	 */
 	private int GUID;
+	
+	/**
+	 * counts how many people are currently requesting the song
+	 */
 	public int count;
+	
+	/**
+	 * mp3 file of the song represented by bytes
+	 */
 	private byte[] bytes;
 	
+	/**
+	 * Constructor for cached song
+	 * @param id	given guid of the song
+	 * @param b		byte array of the song
+	 */
 	public CachedSong(int id, byte[] b)
 	{
 		GUID = id;
@@ -13,21 +34,37 @@ public class CachedSong
 		bytes = b;
 	}
 	
+	/**
+	 * retrieve GUID
+	 * @return		GUID
+	 */
 	public int getGUID()
 	{
 		return GUID;
 	}
 	
+	/**
+	 * sets GUID
+	 * @param g		given GUID
+	 */
 	public void setGUID(int g)
 	{
 		GUID = g;
 	}
 	
+	/**
+	 * retrieves byte array
+	 * @return		byte array of the song
+	 */
 	public byte[] getBytes()
 	{
 		return bytes;
 	}
 	
+	/**
+	 * sets byte array
+	 * @param b		byte array of the song
+	 */
 	public void setBytes(byte[] b)
 	{
 		bytes = b;
