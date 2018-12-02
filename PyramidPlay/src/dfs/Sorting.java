aafe108b1f9ec300384106cd767b283be21a9b00
Wesley Slates <Wesley.Slates@student.csulb.edu>
2018-12-02 14:53:12 -0800

    Sorting framework added

---
+++ PyramidPlay/src/dfs/Sorting.java
@@ -0,0 +1,18 @@
+package dfs;
+
+public class Sorting {
+
+	public File mapReduce (File file) {
+		return null;
+	}
+	
+	private File map(File file) {
+		return null;
+	}
+	
+	private File reduce(File file) {
+		return null;
+	}
+	
+	
+}
