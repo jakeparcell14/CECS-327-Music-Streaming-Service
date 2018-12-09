294166b8bf17e755308e77aced5dd34314d28a31
jakeparcell14 <jakeparcell14@gmail.com>
2018-12-09 12:47:21 -0800

    Added Reduce function and threading

--- PyramidPlay/src/dfs/Sorting.java
+++ PyramidPlay/src/dfs/Sorting.java
@@ -1,10 +1,14 @@
 package dfs;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
+import com.google.gson.Gson;
 
 import application.Song;
 import p2p.PeerToPeer;
@@ -26,9 +30,83 @@ public class Sorting {
 		return null;
 	}
 	
-	private File reduce(File file) {
-		return null;
+
+	private static class Handler extends Thread
+	{
+		private Chunk chunk;
+		private File f;
+		private Gson gson;
+
+		public Handler(File givenFile, Chunk c)
+		{
+			this.chunk = c;
+			this.f = givenFile;
+			gson = new Gson();
+		}
+
+		public void run()
+		{
+			//get the songs from a specific chunk
+			Song[] temp=gson.fromJson(new String(chunk.getData()).trim(), Song[].class);
+
+			ArrayList<Song> updatedSongs = new ArrayList<Song>();
+
+			for(int i = 0; i < temp.length; i++)
+			{
+				if(!updatedSongs.contains(temp[i]))
+				{
+					//song being tested is not a duplicate
+					updatedSongs.add(temp[i]);
+				}
+			}
+
+			//sort songs from chunk
+			Collections.sort(updatedSongs);
+
+			byte[] newChunk =  gson.toJson((Song[]) updatedSongs.toArray(new Song[updatedSongs.size()]), Song[].class).getBytes();
+
+			try 
+			{
+				//add reduced chunk to file
+				f.append(newChunk);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
+
+	private File reduce(File file) {
+		//get chunks from file, get file data, add to treemap to remove duplicates, sort map, overwrite onto peer
+		File updatedFile = new File(file.getFileName());
+		Gson gson = new Gson();
+		
+		ArrayList<Handler> threads = new ArrayList<Handler>();
+
+		//reduce each chunk in its own thread
+		for(int i = 0; i < file.getNumberOfChunks(); i++)
+		{
+			Handler h = new Handler(updatedFile, file.getChunk(i));
+
+			threads.add(h);
+			
+			h.start();
+		}
+		
+		for(Handler thread : threads)
+		{
+			try 
+			{
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return updatedFile;
+	}
+
 	
 	private void emitMap(Integer key, String value, Counter counter) {
 		if (value.compareTo("A") >= 0 && value.compareTo("L") <= 0) {
@@ -50,7 +128,7 @@ public class Sorting {
 		counter.decrement();
 	}
 	
-	private void mapContext(Integer page, MapInterface mapper, Counter counter) {
+/*	private void mapContext(Integer page, MapInterface mapper, Counter counter) {
 		PeerToPeer p2p = PeerToPeer.getInstance();
 		Chunk ch = (Chunk) p2p.Get(page);
 		
@@ -77,7 +155,7 @@ public class Sorting {
 		counter.increment(page, n);
 		
 		
-	}
+	}*/
 	
 	
 }
